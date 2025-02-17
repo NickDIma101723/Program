c6b6a02dcf3334b11e9a78a20e0d6f63f475f4b5
NickDIma101723 <101723@glr.nl>
2025-02-17 22:24:35 +0000

    Website volledig afgemaakt

--- Home/js/script.js
+++ Home/js/script.js
@@ -1,21 +1,29 @@
 const images = [
-
+    'images/image1.png', 
+    'images/image2.png', 
+    'images/image3.png'
 ];
 
 let currentImageIndex = 0;
 
 function showNextImage() {
-
     currentImageIndex++;
     if (currentImageIndex >= images.length) {
         currentImageIndex = 0;
     }
-
-    console.log("Current image:", images[currentImageIndex]);
 
     document.getElementById("slider").src = images[currentImageIndex];
 
     setTimeout(showNextImage, 10000);
 }
 
-window.onload = showNextImage;
+function playMusic() {
+    const music = document.getElementById("background-music");
+    music.play();
+
+   setTimeout(() => {
+        music.pause();
+    }, 30000);
+}
+
+showNextImage();
